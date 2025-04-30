6c0fd5dbb8aebf16f73b1891bf5c4cde540ea5c6
Berkay Alkan <berky.alknn@gmail.com>
2025-04-30 23:56:38 +0300

    Configure notes.js to be imported and rendered in App.jsx

---
+++ src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+    {
+      key: 1,
+      title: "Buy Groceries",
+      content:
+        "Milk, Bread, Eggs, and Fruits"
+    },
+    {
+      key: 2,
+      title: "Study React",
+      content:
+        "Review props, state, and hooks"
+    },
+    {
+      key: 3,
+      title: "Workout",
+      content:
+        "Go for a 30-minute run and do strength training"
+    },
+    {
+      key: 4,
+      title: "Clean Desk",
+      content:
+        "Organize books and wipe down surfaces"
+    }
+  ];
+
+  export default notes;
