6dd125525592f7168f4c8a97cd1b7d68d1f7774a
Berkay Alkan <berky.alknn@gmail.com>
2025-05-20 22:39:04 +0300

    Enable adding notes via CreateArea component and manage state in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from "../notes";
+import CreateArea from './CreateArea';
 
 
 function App() {
 
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => [...prevNotes, newNote]);
+  }
+
   return (
     <div>
-        <Header />
-        {notes.map( (noteItem) => <Note 
-                key = {noteItem.key} 
-                title = {noteItem.title}
-                content = {noteItem.content} 
-              />
-        )}
-        <Footer />
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => (
+        <Note key={index} title={noteItem.title} content={noteItem.content} />
+      ))}
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
